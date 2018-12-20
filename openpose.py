4268ab4cce8833f081ec5ccaa7bbaa5eb768354f
FLHonker <frankliuceo@163.com>
2018-12-20 23:08:39 +0800

    error indent

--- openpose.py
+++ openpose.py
@@ -10,10 +10,10 @@ parser = argparse.ArgumentParser(
         description='This script is used to demonstrate OpenPose human pose estimation network '
                     'from https://github.com/CMU-Perceptual-Computing-Lab/openpose project using OpenCV. '
                     'The sample and model are simplified and could be used for a single person on the frame.')
-parser.add_argument('--input', help='Path to image or video. Skip to capture frames from camera')
-parser.add_argument('--proto', help='Path to .prototxt')
-parser.add_argument('--model', help='Path to .caffemodel')
-parser.add_argument('--dataset', help='Specify what kind of model was trained. '
+parser.add_argument('--input', default='pbug_1m.mp4', help='Path to image or video. Skip to capture frames from camera')
+parser.add_argument('--proto', default='pose_deploy_linevec.prototxt', help='Path to .prototxt')
+parser.add_argument('--model', default='pose/coco/pose_iter_440000.caffemodel', help='Path to .caffemodel')
+parser.add_argument('--dataset',default='COCO' , help='Specify what kind of model was trained. '
                                       'It could be (COCO, MPI) depends on dataset.')
 parser.add_argument('--thr', default=0.1, type=float, help='Threshold value for pose parts heat map')
 parser.add_argument('--width', default=368, type=int, help='Resize input to specific width.')
@@ -71,6 +71,7 @@ poseFrame = np.zeros((h, w, 3), np.uint8)
 poseFrame.fill(0)
 
 cv.namedWindow('OpenPose')
+
 # 计时开始
 start_time = time.time()
 
@@ -120,21 +121,21 @@ while cv.waitKey(1) != 27:
         idTo = BODY_PARTS[partTo]
 
         if points[idFrom] and points[idTo]:
-            cv.line(poseFrame, points[idFrom], points[idTo], colors[i], 3)
+            cv.line(poseFrame, points[idFrom], points[idTo], (0, 255, 0), 3)
             cv.ellipse(poseFrame, points[idFrom], (3, 3), 0, 0, 360, (0, 0, 255), cv.FILLED)
             cv.ellipse(poseFrame, points[idTo], (3, 3), 0, 0, 360, (0, 0, 255), cv.FILLED)
 
     t, _ = net.getPerfProfile()
     freq = cv.getTickFrequency() / 1000
     cv.putText(frame, '%.2fms' % (t / freq), (10, 20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
-'''
-    neck = points[BODY_PARTS['Neck']]
-    left_wrist = points[BODY_PARTS['LWrist']]
-    right_wrist = points[BODY_PARTS['RWrist']]
-    print(neck, left_wrist, right_wrist)
-    if neck and left_wrist and right_wrist and left_wrist[1] < neck[1] and right_wrist[1] < neck[1]:
-        cv.putText(frame, 'HANDS UP!', (10, 100), cv.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 2)
-'''
+# '''
+#     neck = points[BODY_PARTS['Neck']]
+#     left_wrist = points[BODY_PARTS['LWrist']]
+#     right_wrist = points[BODY_PARTS['RWrist']]
+#     print(neck, left_wrist, right_wrist)
+#     if neck and left_wrist and right_wrist and left_wrist[1] < neck[1] and right_wrist[1] < neck[1]:
+#         cv.putText(frame, 'HANDS UP!', (10, 100), cv.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 2)
+# '''
     cv.imshow('OpenPose', poseFrame)
     poseout.write(poseFrame)
     # cv.imwrite('outpose/pose_%d.jpg'%(j), frame)
@@ -142,8 +143,7 @@ while cv.waitKey(1) != 27:
     if j % 20 == 0:
         # 记录时间
         end_time = time.time()
-        print('已处理{}帧图像， 用时{:.4f}s， 平均每帧用时{:.4f}s'.\
-            format(j, end_time - start_time, (end_time-start_time)/j))
+        print('已处理{}帧图像， 用时{:.4f}s， 平均每帧用时{:.4f}s'.format(j, end_time - start_time, (end_time-start_time)/j))
 
 # 计时结束
 end_time = time.time()
