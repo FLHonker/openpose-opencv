8a21bc6ae486a880181a988e01e9f9b33203b22c
FLHonker <frankliuceo@163.com>
2018-12-20 16:14:59 +0800

    modify

--- openpose.py
+++ openpose.py
@@ -1,8 +1,10 @@
+
 # To use Inference Engine backend, specify location of plugins:
 # export LD_LIBRARY_PATH=/opt/intel/deeplearning_deploymenttoolkit/deployment_tools/external/mklml_lnx/lib:$LD_LIBRARY_PATH
 import cv2 as cv
 import numpy as np
 import argparse
+import time
 
 parser = argparse.ArgumentParser(
         description='This script is used to demonstrate OpenPose human pose estimation network '
@@ -42,21 +44,46 @@ else:
                    ["LElbow", "LWrist"], ["Neck", "Chest"], ["Chest", "RHip"], ["RHip", "RKnee"],
                    ["RKnee", "RAnkle"], ["Chest", "LHip"], ["LHip", "LKnee"], ["LKnee", "LAnkle"] ]
 
+# visualize
+colors = [[255, 0, 0], [255, 85, 0], [255, 170, 0], [255, 255, 0], [170, 255, 0], [85, 255, 0],
+          [0, 255, 0], \
+          [0, 255, 85], [0, 255, 170], [0, 255, 255], [0, 170, 255], [0, 85, 255], [0, 0, 255],
+          [85, 0, 255], \
+          [170, 0, 255], [255, 0, 255], [255, 0, 170], [255, 0, 85]]
+          
 inWidth = args.width
 inHeight = args.height
 
 net = cv.dnn.readNetFromCaffe(args.proto, args.model)
 
 cap = cv.VideoCapture(args.input if args.input else 0)
+# vedio writer
+# fourcc = cv.VideoWriter_fourcc('m', 'p', '4', 'v')
+fourcc = cv.VideoWriter_fourcc(* 'XVID')
+# 保存size必须和输出size设定为一致，否则无法写入保存文件
+w = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
+h = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+size = (w, h)
+poseout = cv.VideoWriter('output.avi', fourcc, 20.0, size)
+#create a black use numpy,size is:512*512
+poseFrame = np.zeros((h, w, 3), np.uint8)   
+#fill the image with white
+poseFrame.fill(0)
 
-while cv.waitKey(1) < 0:
+cv.namedWindow('OpenPose')
+# 计时开始
+start_time = time.time()
+
+j = 0
+while cv.waitKey(1) != 27:
     hasFrame, frame = cap.read()
     if not hasFrame:
-        cv.waitKey()
+        # cv.waitKey()
         break
 
     frameWidth = frame.shape[1]
     frameHeight = frame.shape[0]
+
     inp = cv.dnn.blobFromImage(frame, 1.0 / 255, (inWidth, inHeight),
                               (0, 0, 0), swapRB=False, crop=False)
     net.setInput(inp)
@@ -64,7 +91,9 @@ while cv.waitKey(1) < 0:
 
     # assert(len(BODY_PARTS) == out.shape[1])
 
+    # reset
     points = []
+    poseFrame.fill(0)
     for i in range(len(BODY_PARTS)):
         # Slice heatmap of corresponging body's part.
         heatMap = out[0, i, :, :]
@@ -78,8 +107,10 @@ while cv.waitKey(1) < 0:
 
         # Add a point if it's confidence is higher than threshold.
         points.append((int(x), int(y)) if conf > args.thr else None)
+    # *************** points *********** 
+    # print(points)
 
-    for pair in POSE_PAIRS:
+    for i, pair in POSE_PAIRS:
         partFrom = pair[0]
         partTo = pair[1]
         # assert(partFrom in BODY_PARTS)
@@ -89,19 +120,35 @@ while cv.waitKey(1) < 0:
         idTo = BODY_PARTS[partTo]
 
         if points[idFrom] and points[idTo]:
-            cv.line(frame, points[idFrom], points[idTo], (0, 255, 0), 3)
-            cv.ellipse(frame, points[idFrom], (3, 3), 0, 0, 360, (0, 0, 255), cv.FILLED)
-            cv.ellipse(frame, points[idTo], (3, 3), 0, 0, 360, (0, 0, 255), cv.FILLED)
+            cv.line(poseFrame, points[idFrom], points[idTo], colors[i], 3)
+            cv.ellipse(poseFrame, points[idFrom], (3, 3), 0, 0, 360, (0, 0, 255), cv.FILLED)
+            cv.ellipse(poseFrame, points[idTo], (3, 3), 0, 0, 360, (0, 0, 255), cv.FILLED)
 
     t, _ = net.getPerfProfile()
     freq = cv.getTickFrequency() / 1000
     cv.putText(frame, '%.2fms' % (t / freq), (10, 20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
-
+'''
     neck = points[BODY_PARTS['Neck']]
     left_wrist = points[BODY_PARTS['LWrist']]
     right_wrist = points[BODY_PARTS['RWrist']]
     print(neck, left_wrist, right_wrist)
     if neck and left_wrist and right_wrist and left_wrist[1] < neck[1] and right_wrist[1] < neck[1]:
         cv.putText(frame, 'HANDS UP!', (10, 100), cv.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 2)
+'''
+    cv.imshow('OpenPose', poseFrame)
+    poseout.write(poseFrame)
+    # cv.imwrite('outpose/pose_%d.jpg'%(j), frame)
+    j += 1
+    if j % 20 == 0:
+        # 记录时间
+        end_time = time.time()
+        print('已处理{}帧图像， 用时{:.4f}s， 平均每帧用时{:.4f}s'.\
+            format(j, end_time - start_time, (end_time-start_time)/j))
 
-    cv.imshow('OpenPose using OpenCV', frame)
+# 计时结束
+end_time = time.time()
+print('共处理{}帧图像，用时{:.4f}s'.format(j, end_time - start_time))
+
+cap.release()
+poseout.release()
+cv.destroyAllWindows()
